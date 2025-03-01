a2bf45c6737b13feca9e051e06a4c9d438ca1513
Brandon <32753167+btoms20@users.noreply.github.com>
2025-02-28 21:34:06 -0800

    formatted

--- Package.swift
+++ Package.swift
@@ -22,17 +22,18 @@ let package = Package(
         .macOS(.v10_15),
         .iOS(.v13),
         .watchOS(.v6),
-        .tvOS(.v13)
+        .tvOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Noise",
-            targets: ["Noise"]),
+            targets: ["Noise"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -40,10 +41,12 @@ let package = Package(
         .target(
             name: "Noise",
             dependencies: [
-                .product(name: "Crypto", package: "swift-crypto"),
-            ]),
+                .product(name: "Crypto", package: "swift-crypto")
+            ]
+        ),
         .testTarget(
             name: "NoiseTests",
-            dependencies: ["Noise"]),
+            dependencies: ["Noise"]
+        ),
     ]
 )
